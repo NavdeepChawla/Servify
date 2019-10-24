c57fbf3262393ce6f04f91224c90c22846771bf5
NavdeepChawla <navdeepchawla2000@gmail.com>
2019-10-25 04:57:56 +0530

    Final Commit

---
+++ app/src/main/java/com/example/servify/DetectionActivity.java
@@ -0,0 +1,14 @@
+package com.example.servify;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
